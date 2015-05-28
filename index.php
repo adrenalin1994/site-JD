6cde81cfae5bac1017f5921776fd0bb7d9a2390c
adrenalin <gaidairoma@mail.ru>
2015-05-28 15:35:16 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+
+<HTML>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-md-2">
+      <img src="http://kaskad71.ru/upload/iblock/75e/75eff4a31c19493095c179b614d34618.jpg" class="img-responsive" alt="Responsive image"> </div> 
+  <div class="col-md-10"> 
+      <br> <br> 
+    <ul class="nav nav-tabs">
+  <li class="active"><a href="index.php">Ãëàâíàÿ</a></li>
+  <li><a href="Kompania.php">Î êîìïàíèè</a></li>
+  <li><a href="Rukovodstvo.php"> Ðóêîâîäñòâî</a></li>
+  <li><a href="Novosti.php">Íîâîñòè</a></li>
+  <li><a href="RJD.php">ÐÆÄ ñåãîäåíÿ</a></li>
+  <li><a href="Kontakti.php">Êîíòàêòû</a></li>
+          <form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="ÂâåäèòåÒåêñò">
+  </div>
+  <button type="submit" class="btn btn-default">Ïîèñê</button>
+</form>
+</ul> </div>
+  
+    </div>
+</div>
+
+
+ <div class="container">
+<div class="row">
+    <div class="col-md-2">  
+<br> 
+        <p class="text-center"> <b> ÏÀÑÑÀÆÈÐÀÌ!  </b> </p>  
+<p class="text-center"> ÐÀÑÏÈÑÀÍÈÅ, ÍÀËÈ×ÈÅ ÌÅÑÒ, ÏÎÊÓÏÊÀ ÁÈËÅÒÎÂ </p>
+        
+        <input type="text" class="form-control" placeholder="Îòêóäà">
+
+        <input type="text" class="form-control" placeholder="Êóäà">
+
+<form>
+   <p> 
+   <input type="date" class="form-control" name="calendar" value="2015-05-24"
+    max="2015-12-31" min="2015-01-1">
+   </p>
+  </form>
+
+        <button type="submit" class="btn btn-default">Èñêàòü</button>
+        
+    </div>
+    <div class="col-md-10"> <img src="https://www.tomslatin.com/wp-content/uploads/Junked-Train-Cars-Panorama-Edit.jpg" class="img-responsive" alt="Responsive image"> 
+	<br>
+ <h3> <p class="text-center"> <b>  ÍOÂÎÑÒÈ ÑÅÃÎÄÍß </b> </p> </h3>
+<br>
+ <h4> <p class="text-center"> <b>  Èííîâàöèîííîå ðàçâèòèå ÎÀÎ "ÐÆÄ" </b> </p> </h4>
+
+<br>
+  <p class="text-justify"> Öåëü èííîâàöèîííîãî ðàçâèòèÿ êîìïàíèè - äîñòèæåíèå ýôôåêòèâíîñòè ðåçóëüòàòîâ ïðè ïîñòîÿííîì ðîñòå êà÷åñòâà ïðåäîñòàâëÿåìûõ óñëóã, âûñîêîì óðîâíå èííîâàöèé è áåçîïàñíîñòè ïåðåâîçîê.  </p> 
+  <p class="text-justify"> Â 2013 ãîäó âî èñïîëíåíèå ðåøåíèÿ ïðîòîêîëà èòîãîâîãî çà 2012 ãîä çàñåäàíèÿ ïðàâëåíèÿ ÎÀÎ "ÐÆÄ" ïðîâåäåíà ðàáîòà ïî àêòóàëèçàöèè Ïðîãðàììû èííîâàöèîííîãî ðàçâèòèÿ ÎÀÎ "ÐÆÄ" íà ïåðèîä äî 2015 ãîäà (äàëåå – àêòóàëèçèðîâàííàÿ Ïðîãðàììà). Â õîäå àêòóàëèçàöèè Ïðîãðàììû óòî÷íåíû ïîêàçàòåëè ýôôåêòèâíîñòè åå ðåàëèçàöèè, ñôîðìèðîâàí ñðåäíåñðî÷íûé ïëàí ïî åå ðåàëèçàöèè, â ñîñòàâ êîòîðîãî âîøëè ìåðîïðèÿòèÿ, â òîì ÷èñëå ñâÿçàííûå ñ îñâîåíèåì íîâîé òåõíèêè è òåõíîëîãèé, ÷òî ïðåäïîëàãàåò ïîâûøåíèå ïðîèçâîäèòåëüíîñòè òðóäà, à òàêæå ñîçäàíèå è ìîäåðíèçàöèþ âûñîêîïðîèçâîäèòåëüíûõ ðàáî÷èõ ìåñò, â ïåðâóþ î÷åðåäü, ó ïðîèçâîäèòåëåé ýòîé òåõíèêè. </p>
+	</div>
+    
+
+</div>
+</div>
+
+</body>
+</html>
+<!-- Новое изменение файла для курсового проекта -->
